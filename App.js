bf4bb0e2dbbf10207eb20da895bf869e46a30084
srivastavarahul442 <srivastavarahul442@gmail.com>
2024-09-13 02:48:42 +0530

    Learning -  Basic of React (JSX, Functional Comp.)

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const node = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "parent1" }, [
-    React.createElement("h1", { id: "child1" }, "hello-child1"),
-    React.createElement("h1", { id: "child2" }, "hello-child2"),
-  ]),
-  React.createElement("div", { id: "parent2" }, [
-    React.createElement("h1", { id: "child1" }, "hello-child1"),
-    React.createElement("h1", { id: "child2" }, "hello-child2"),
-    React.createElement("h1", { id: "child2" }, "hello-child3"),
-  ]),
-]);
+// React.createElement => ReactElement-JS (Object) => HTMLElement(render)
+const heading = React.createElement(
+  "div",
+  { id: "head" },
+  React.createElement("h1", { id: "h1" }, "Hello Rahul...")
+);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// JSX, HTML like syntax
+// JSX (transpiled before it reaches the JS) - Parcel - Babel
+// JSX =>Babel transpiles to React.createElement => ReactElement-JS (Object) => HTMLElement(render)
+const jsxHeading = <h1>Hello... From JSX</h1>
 
-root.render(node);
+
+//component
+//class based component --- Old
+//functional based component --- New
+
+const num = 10000;
+const HeadComp1 = () => (
+  <h1>Hello from HeadComp1 {num}</h1>
+)
+
+
+const HeadComp = () => {
+  return <div>
+    {/* we can call another component inside any component which is know as component composition */}
+    <HeadComp1 />
+    <HeadComp1></HeadComp1>
+    {HeadComp1()}
+    {/* we can use ReactElement inside JSX inside the {} bracket */}
+    <div>{heading}</div> 
+    <h1>Hello From HeadComp</h1>
+  </div>
+}
+
+<HeadComp />
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<HeadComp />); 
