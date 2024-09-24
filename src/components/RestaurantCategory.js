74b78596faf2b8af2e1ecb3bbef769d04f914385
srivastavarahul442 <srivastavarahul442@gmail.com>
2024-09-24 23:44:57 +0530

    Learning - Higher order components, controlled and uncontrolled components, lifting the state up

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import ListItem from "./ListItem";
 
-const RestaurantCategory = (prop) => {
-  const { title, itemCards } = prop.resDetail;
-    // console.log(itemCards);
+const RestaurantCategory = ({resDetail,showItem,setShowIndex}) => {
+  const { title, itemCards } = resDetail;
+  
+  const handledClick = () =>{
+    setShowIndex()
+  }
+
   return (
     <div>
       <div className="w-6/12 mx-auto my-4 p-4 shadow-lg bg-gray-50 ">
-        <div className="flex justify-between">
+        <div onClick={handledClick} className="flex justify-between cursor-pointer">
           <span className="font-bold text-lg">
             {title} ({itemCards.length})
           </span>
           <span>🔽</span>
         </div>
-        <ListItem items={itemCards}/>
+        {
+          showItem && <ListItem items={itemCards}/>
+        }
       </div>
     </div>
   );
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import ShimmerUI from "./ShimmerUI";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
-import RestaurantCategory from "./RestaurantCategory"
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
+
+  const [showIndex, setShowIndex]= useState(0)
+
   const { resID } = useParams();
 
   const resInfo = useRestaurantMenu(resID);
@@ -32,9 +35,14 @@ const RestaurantMenu = () => {
       <p className="font-bold text-lg">
         {cuisines.join(", ")} - {costForTwoMessage}
       </p>
-      {
-        categories.map((res)=><RestaurantCategory resDetail ={res?.card?.card}/>)
-      }
+      {categories.map((res,index) => (
+        <RestaurantCategory
+          key={res?.card?.card?.title}
+          resDetail={res?.card?.card}
+          showItem={index===showIndex?true:false}
+          setShowIndex={()=>{setShowIndex(index)}}
+        />
+      ))}
     </div>
   );
 };
